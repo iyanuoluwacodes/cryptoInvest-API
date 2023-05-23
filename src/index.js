d60ca436a88bab9f0e4b102df483b1818af65c4c
iycodes <iyanuoluwa.code@gmail.com>
2023-05-23 14:37:46 +0100

    .

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ function commitToDB(promise) {
     });
 }
 exports.app.listen({
-    port: 3000,
+    port: Number(process.env.PORT) || 3000,
 }, function (err, address) {
     if (err) {
         console.log(err);
